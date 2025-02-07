c2d8425787fb14706d6b9ffb2968e8ae20d115e7
kskscham <155929670+kskscham@users.noreply.github.com>
2025-02-07 18:41:37 +0800

    Add files via upload

---
+++ Flower Shop/footer.php
@@ -0,0 +1,39 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="contact.php">Contact</a>
+            <a href="shop.php">Shop</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="orders.php">My orders</a>
+            <a href="cart.php">My cart</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <p> <i class="fas fa-phone"></i> 09648152791 </p>
+            <p> <i class="fas fa-phone"></i> 09794313432 </p>
+            <p> <i class="fas fa-envelope"></i>flowers@gmail.com </p>
+            <p> <i class="fas fa-map-marker-alt"></i> Batangas, Lipa City </p>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>facebook</a>
+            <a href="#"><i class="fab fa-twitter"></i>twitter</a>
+            <a href="#"><i class="fab fa-instagram"></i>instagram</a>
+            <a href="#"><i class="fab fa-linkedin"></i>linkedin</a>
+        </div>
+
+        </div>
+
+    <div class="credit"> <?php echo date('Y');  ?>  <span>  All rights reserved</span> </div>
+
+</section>
